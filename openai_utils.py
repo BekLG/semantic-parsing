aebce6944c608cdd309094dc5c41e99d299c5dac
BekLg <bereketlegesse22@gmail.com>
2025-02-11 18:38:43 +0300

    simple modification to add the api key

--- openai_utils.py
+++ openai_utils.py
@@ -1,15 +1,17 @@
 import os
-
+from dotenv import load_dotenv
 from openai import OpenAI
 
+load_dotenv()
 
+print(os.getenv('OPENAI_API_KEY'))
 
 def openai_generate(messages: list, model: str = 'gpt-4', **generation_params):
-    client = OpenAI()
+    client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))  # Fixed initialization
 
     completion = client.chat.completions.create(
         messages=messages,
         model=model,
         **generation_params
     )
-    return completion#.choices[0].message.content
+    return completion
